2cdda9f1d2e0477c84500b331ca8ca905a23267d
Tee787 <tawhiri.tamihana@gmail.comgit config --global user.name Tawhirimatea>
2018-09-20 19:57:51 +1200

    first commit

---
+++ Arrays/Arrays/Program.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("*******************************************************");
+            //Console.WriteLine("**************************Task 1***********************");
+            //Console.WriteLine("*******************************************************");
+
+            //string[] names = new String[3];
+            //Console.WriteLine("Enter the name of the gold medal winner: ");
+            //names[0] = Console.ReadLine();
+            //Console.WriteLine("Enter the name of the silver medal winner: ");
+            //names[1] = Console.ReadLine();
+            //Console.WriteLine("Enter the name of the bronze medal winner: ");
+            //names[2] = Console.ReadLine();
+
+            //Console.WriteLine("\nWhich medal winner would you like to know: ");
+            //Console.WriteLine("Press 1 for the gold winner: ");
+            //Console.WriteLine("Press 2 for the gold winner: ");
+            //Console.WriteLine("Press 3 for the gold winner: ");
+            //int input = int.Parse(Console.ReadLine());
+            //if(input == 1)
+            //{
+            //    Console.WriteLine($"Gold medal went to: {names[input - 1]}");
+            //}
+            //else if(input == 2)
+            //{
+            //    Console.WriteLine($"Silver medal went to: {names[input - 2]}");
+            //}
+            //else if(input == 3)
+            //{
+            //    Console.WriteLine($"Bronze medal went to: {names[input - 3]}");
+            //}
+
+            //Console.ReadLine();
+
+            //Console.WriteLine("*******************************************************");
+            //Console.WriteLine("**************************Task 2***********************");
+            //Console.WriteLine("*******************************************************");
+
+            //string[] names = new string[5];
+
+            /*Console.WriteLine($"Please enter the first name:\n ");        //Using standard method to start our program
+            names[0] = Console.ReadLine();
+            Console.WriteLine($"\nPlease enter the second name:\n ");
+            names[1] = Console.ReadLine();
+            Console.WriteLine($"\nPlease enter the third name:\n ");
+            names[2] = Console.ReadLine();
+            Console.WriteLine($"\nPlease enter the forth name:\n ");
+            names[3] = Console.ReadLine();
+            Console.WriteLine($"\nPlease enter the fifth name:\n ");
+            names[4] = Console.ReadLine();*/
+
+            /*for (int i = 0; i <= 4; i++)                                    //for loop used to shorten the standard method
+            {
+                Console.WriteLine($"\nPlease enter name {i + 1}:\n ");      //Here is where the input names are added to intiate the for loop
+            }
+            Console.WriteLine($"\nThe first name entered is {names[0]}");   //Output of the final result
+            Console.WriteLine($"\nThe last name entered is {names[4]}");
+
+            Console.ReadLine();*/
+
+            //Console.WriteLine("*******************************************************");
+            //Console.WriteLine("**************************Task 3***********************");
+            //Console.WriteLine("*******************************************************");
+
+            //string[] names = { "Jim", "John", "Max", "Tarnz" };
+            //Random rand = new Random();
+
+            //int winner = rand.Next(0, 4);
+            //Console.WriteLine($"{names[winner]}");
+
+            //Console.ReadLine();
+
+
+            //Console.WriteLine("*******************************************************");
+            //Console.WriteLine("**************************Task 4***********************");
+            //Console.WriteLine("*******************************************************");
+
+            //string[] greetings = {"Hello", "KiaOra", "Kon Nichiwa", "Namaste", "Bonjour", "Talofa", };
+            //Console.WriteLine("Please select a language.....");
+            //Console.WriteLine("Press 1 for Moari.....");
+            //Console.WriteLine("Press 2 for Japanese.....");
+            //Console.WriteLine("Press 3 for Hindi.....");
+            //Console.WriteLine("Press 4 for French.....");
+            //Console.WriteLine("Press 5 for Samoan.....");
+            //int selection = int.Parse(Console.ReadLine());
+
+            //switch (selection)
+            //{
+            //    case 1:
+            //        Console.WriteLine(greetings[selection - 1]);
+            //        break;
+            //    case 2:
+            //        Console.WriteLine(greetings[selection - 1]);
+            //        break;
+            //    case 3:
+            //        Console.WriteLine(greetings[selection - 1]);
+            //        break;
+            //    case 4:
+            //        Console.WriteLine(greetings[selection - 1]);
+            //        break;
+            //    case 5:
+            //        Console.WriteLine(greetings[selection - 1]);
+            //        break;
+            //    default:
+            //        Console.WriteLine("Oops, something went wrong...");
+            //        break;
+
+            //}
+
+            //Console.ReadLine();
+        }
+    }
+}
---
+++ Arrays2/Arrays2/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("************************************************************************************");
+            Console.WriteLine("*************************************Reverse Array**********************************");
+            Console.WriteLine("************************************************************************************");
+
+            string[] number = new String[5];
+            Console.WriteLine("\nEnter 5 numbers....");
+            Console.WriteLine("Enter number 1....");
+            number[0] = Console.ReadLine();
+            Console.WriteLine("Enter number 2....");
+            number[1] = Console.ReadLine();
+            Console.WriteLine("Enter number 3....");
+            number[2] = Console.ReadLine();
+            Console.WriteLine("Enter number 4....");
+            number[3] = Console.ReadLine();
+            Console.WriteLine("Enter number 5....");
+            number[4] = Console.ReadLine();
+
+            for  (int i = 0; i <= 5; i++)
+            {
+                Console.WriteLine(number[0] [1],[2],);
+            }
+            for(int i = 5; i >= 0; i--)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("This is my reverse array.");
+
+            /*********************************************************************************************/
+
+            /*Console.WriteLine("********************************************************************");
+            Console.WriteLine("********************************************************************");
+            Console.WriteLine("********************************************************************");
+
+            Console.WriteLine("Please enter how big an array you would like: ");    //Console display output
+            int input = int.Parse(Console.ReadLine());                              //input variable used plus integer convert statement
+            int total = 0;                                                          //total variable used
+            int[] array1 = new int[input];                                          //array integer input
+
+            Option1(input, total, array1);                                          //Option1 Method
+            Console.Clear();
+            Option2(input, total, array1);                                          //Option2 Method
+        }
+        public static void Option1(int input, int total, int[] array1)              //Calling Method for Option1
+        {
+            for (int i = 0; i < input; i++)                                         //for loop statement used in Option1
+            {
+                Console.Write($"Please enter number {i + 1}: ");                    //Write method used for user input of integer
+                array1[i] = int.Parse(Console.ReadLine());                          //output of array1 loop. Converted to an integer
+            }
+
+            for (int i = 0; i < input; i++)                                          //second for loop statement used
+            {
+                Console.Write($"Number {i + 1}: array1[i]\n");                      //user input of integer
+                total += array1[i];
+            }
+            Console.WriteLine($"total = {total}");
+        }
+
+        public static void Option2(int input, int total, int[] array1)              //Calling Method for Option2
+        {
+            for (int i = 0; i < input; i++)                                         //for loop statement usage in Option2
+            {
+                Console.Write($"Number {i + 1}: array1[i]\n");
+                total += array1[i];
+            }
+            
+            foreach(int x in array1)                                                //foreach statement used
+            {
+                Console.WriteLine(x);
+                total += x;
+            }
+            Console.WriteLine($"total = {total}");
+            Console.ReadLine();*/
+        }
+
+    }
+}
